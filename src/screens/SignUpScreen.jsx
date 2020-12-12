993a21042940c8b2717b6afb3599ed0b9c9c4c80
shunwitter <3123900+shunwitter@users.noreply.github.com>
2020-12-12 15:31:56 +0900

    Add Loading compnent to SignUpScreen

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -5,13 +5,16 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function SignUpScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   function handlePress() {
+    setLoading(true);
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -24,11 +27,15 @@ export default function SignUpScreen(props) {
       .catch((error) => {
         console.log(error.code, error.message);
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }
 
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
         <TextInput
